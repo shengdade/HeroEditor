465f32b8a7bd97724272046f6ee933b148353e56
Dade Sheng <dade@live.cn>
2017-04-10 14:58:53 -0400

    http

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,21 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
+import {Component, OnInit} from "@angular/core";
+import {Hero} from "./hero";
+import {HeroService} from "./hero.service";
 
 @Component({
-  selector: 'my-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: [ './dashboard.component.css' ]
+    selector: 'my-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
-  heroes: Hero[] = [];
+    heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+    constructor(private heroService: HeroService) {
+    }
 
-  ngOnInit(): void {
-    this.heroService.getHeroes()
-      .then(heroes => this.heroes = heroes.slice(1, 5));
-  }
+    ngOnInit(): void {
+        this.heroService.getHeroes()
+            .then(heroes => this.heroes = heroes.slice(1, 5));
+    }
 }
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,35 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
+import {Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
+import {Hero} from "./hero";
+import {HeroService} from "./hero.service";
 
 @Component({
-  selector: 'my-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: [ './heroes.component.css' ]
+    selector: 'my-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[];
-  selectedHero: Hero;
+    heroes: Hero[];
+    selectedHero: Hero;
 
-  constructor(
-    private router: Router,
-    private heroService: HeroService) { }
+    constructor(private router: Router,
+                private heroService: HeroService) {
+    }
 
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
 
-  ngOnInit(): void {
-    this.getHeroes();
-  }
+    ngOnInit(): void {
+        this.getHeroes();
+    }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 
-  gotoDetail(): void {
-    this.router.navigate(['/detail', this.selectedHero.id]);
-  }
+    gotoDetail(): void {
+        this.router.navigate(['/detail', this.selectedHero.id]);
+    }
 }
